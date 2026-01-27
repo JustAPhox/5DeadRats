b3e89b2fc4d5f08282ff9fcbe7df263385e493d2
JustAPhox <justafirephox@gmail.com>
2026-01-27 10:27:41 +0000

    More stuff
    Added Separate Quiz Ui from debug menus
    Started work on temp quiz player framework

--- 5DeadRats/Assets/Scripts/MenuLogic.cs
+++ 5DeadRats/Assets/Scripts/MenuLogic.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuLogic : MonoBehaviour
 {
     public GameObject titleScreen;
+    public Slider QuizPlayerCount;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,10 @@ public class MenuLogic : MonoBehaviour
     {
         // Loads the scene
         SceneManager.LoadScene("Quiz Scene", LoadSceneMode.Additive);
+
+
+
+        QuizPlayerCount.value = 2;
 
         // Hides title screen
         titleScreen.SetActive(false);
