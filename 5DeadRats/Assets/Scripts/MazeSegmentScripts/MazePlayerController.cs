03330c85375aa3427c89a266b577b3acd1ec1a8f
JustAPhox <justafirephox@gmail.com>
2026-03-17 11:10:21 +0000

    Added Pausemenu to Quiz

--- 5DeadRats/Assets/Scripts/MazeSegmentScripts/MazePlayerController.cs
+++ 5DeadRats/Assets/Scripts/MazeSegmentScripts/MazePlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
